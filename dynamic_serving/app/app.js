408e9099cfac728d3d43d068a741d513f03ea038
Martin Gottweis <gottwik@gmail.com>
2017-01-17 17:23:37 +0100

    added dynamic serving

---
+++ dynamic_serving/app/app.js
@@ -0,0 +1,12 @@
+var local_app = function () {}
+
+local_app.prototype.init = function (app) {
+	app.get('/random', function (req, res) {
+		enduro.temper.render('random', {random_number: Math.random()})
+			.then((output) => {
+				res.send(output)
+			})
+	})
+}
+
+module.exports = new local_app()
