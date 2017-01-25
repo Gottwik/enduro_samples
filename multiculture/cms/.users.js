d6bfe24fcfa0b02d7dfd3a8be68a26e30be8fa7f
Martin Gottweis <gottwik@gmail.com>
2017-01-25 15:55:38 +0100

    added multiculture sample

---
+++ multiculture/cms/.users.js
@@ -0,0 +1,11 @@
+{
+	users: [
+		{
+			username: 'gottwik',
+			tags: [],
+			salt: '0759aa5426b9759c7b62c89899588d5a',
+			hash: 'e53cefbab9da819c4e36804e01c652cc4230e0dc96698cfb137c31c554c7e9c6',
+			user_created_timestamp: 1485356017539
+		}
+	]
+}
---
+++ multiculture/cms/index.js
@@ -0,0 +1,6 @@
+{
+	greeting: 'Hello',
+	superlative: 'nice',
+	$greeting_de: 'Guten tag',
+	$greeting_fr: 'Bonne journée'
+}
