ae538a05dcdcd644d31b37077eb0832b758ff15d
Martin Gottweis <gottwik@gmail.com>
2017-03-03 15:10:21 +0100

    upgraded to enduro 1.3.0

--- dynamic_serving/app/app.js
+++ dynamic_serving/app/app.js
@@ -1,10 +1,8 @@
 var local_app = function () {}
 
-var temper = enduro.temper
-
 local_app.prototype.init = function (app) {
 	app.get('/random', function (req, res) {
-		temper.render('random', {random_number: Math.random()})
+		enduro.api.temper.render('random', { random_number: Math.random() })
 			.then((output) => {
 				res.send(output)
 			})
