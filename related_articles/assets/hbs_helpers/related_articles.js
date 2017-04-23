4dd2978ca0fa1e3007c4bf246ff291bf0d718b97
Martin Gottweis <martin.gottweis@gmail.com>
2017-04-23 21:22:40 +0200

    added comments

--- related_articles/assets/hbs_helpers/related_articles.js
+++ related_articles/assets/hbs_helpers/related_articles.js
@@ -2,30 +2,37 @@ var Promise = require('bluebird')
 
 enduro.templating_engine.registerHelper('related_articles', function (options) {
 
-	// will store the actual rendered html
+	// store the actual rendered html
 	// alternativelly a each helper could be used
 	var rendered_html = ''
 
-	// will store the promises to read related articles
+	// store the promises to read related articles
 	var related_article_promises = []
 
+	// go throgh all relate articles and load them
 	for (index in this.related_articles) {
 
-		// will store the related article
+		// store the related article
 		var related_article = this.related_articles[index]
 
-		// will load the actual content of the article
+		// load the actual content of the article
 		var related_article_promise = enduro.api.flat.load('/generators/article/' + related_article.related_article_id)
 			.then((related_article_context) => {
+
+				// generate the actual html code
 				return options.fn(related_article_context)
 			})
 			.then((rendered_html_block) => {
+
+				// add the html code together with the previous ones
 				rendered_html += rendered_html_block
 			})
 
+		// add whole thing to a array of promises
 		related_article_promises.push(related_article_promise)
 	}
 
+	// return whole html block upon reading all related articles
 	return Promise.all(related_article_promises)
 		.then(() => {
 			return rendered_html
