2716115cad7e23913ef8cb66d415e02ca43c4052
Martin Gottweis <martin.gottweis@booking.com>
2017-07-11 12:55:54 +0200

    added generator sample

---
+++ generators/cms/.users.js
@@ -0,0 +1,11 @@
+{
+	users: [
+		{
+			username: 'gottwik',
+			tags: [],
+			salt: '235f2b2a0edcb8b34ccb500a163ccb61',
+			hash: 'e6cd2c3d6c932b90c569fbba7291bfe1287869dd940963d2e00f1b8621e6e8a6',
+			user_created_timestamp: 1499765047836
+		}
+	]
+}
