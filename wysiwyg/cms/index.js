4ac4477a00f00f1d31706991f2f18033da6f2f06
Martin Gottweis <martin.gottweis@booking.com>
2017-07-16 03:22:02 +0200

    added sample text

--- wysiwyg/cms/index.js
+++ wysiwyg/cms/index.js
@@ -1,4 +1,4 @@
 {
 	$wysiwyg_customtype: 'wysiwyg',
-	wysiwyg: '<h1>whaaat</h1><div><b>asdf</b></div>'
+	wysiwyg: '<h1>This is a wysiwyg demo</h1><h1><b style="font-size: 16px;">what can this do?</b></h1><div><ol><li>lists<br></li><li><b>bold text</b></li><li><b><strike>strikethrough</strike></b></li><li><i>other stuff</i></li></ol></div>'
 }
