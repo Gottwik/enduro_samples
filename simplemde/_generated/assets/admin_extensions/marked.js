45adbb47318757767362fa016caa2fc74f7f5c02
Cosmo Myzrail Gorynych aka Cos <admin@nersta.ru>
2017-07-19 20:20:38 +1200

    Add multicultural WYSIWYG sample
    Powered by SimpleMDE and Marked.

---
+++ simplemde/_generated/assets/admin_extensions/marked.js
@@ -0,0 +1,43 @@
+// * ———————————————————————————————————————————————————————— * //
+// * 	markdown directives for SimpleMDE
+// * ———————————————————————————————————————————————————————— * //
+
+$.ajaxSetup({
+	cache: true
+})
+
+// inject SimpleMDE's css into the admin interface
+$('head')
+.append('<link rel="stylesheet" type="text/css" href="/assets/vendor/simplemde/dist/simplemde.min.css">')
+.append(`
+	<style>
+		.control-simplemde .CodeMirror {
+			max-height: 30rem;
+		}
+	</style>
+`)
+
+enduro_admin_app.compileProvider
+	.directive('marked', function() {
+		return {
+			link: function(scope, element, attrs) {
+				$.getScript('/assets/vendor/simplemde/dist/simplemde.min.js', function () {
+					var options = {
+						element: element[0],
+						spellChecker: false
+					}
+					var mde = new SimpleMDE(options)
+					mde.codemirror.on('change', function () {
+						scope.context[scope.terminatedkey] = mde.value()
+					})
+					scope.simplemde = {
+						instance: mde
+					}
+					scope.$watch('current_culture', function () {
+						mde.value(scope.context[scope.terminatedkey] || '');
+					})
+				})
+			}
+		}
+	}
+)
---
+++ simplemde/_generated/assets/hbs_templates/end.js
@@ -0,0 +1,3 @@
+define(["handlebars"], function(Handlebars) { return Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<p>Try it out yourself</p>";
+},"useData":true}); });
