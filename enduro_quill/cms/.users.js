ffced0db842614f08873eaf5f489bbd50c27ee35
Martin Gottweis <martin.gottweis@booking.com>
2018-01-07 00:55:12 +0100

    added enduro quill sample

---
+++ enduro_quill/cms/.users.js
@@ -0,0 +1,11 @@
+{
+	users: [
+		{
+			username: 'gottwik',
+			tags: [],
+			salt: 'ad38b8e5f7167bd67e143b68243167c6',
+			hash: '23e5c54b1b5de893ead066e0b3b1c181f0ce7cb5931a6fd83fd4be789dfdd791',
+			user_created_timestamp: 1515279984174
+		}
+	]
+}
---
+++ enduro_quill/cms/index.js
@@ -0,0 +1,4 @@
+{
+	$content_type: 'quill',
+	content: ''
+}
